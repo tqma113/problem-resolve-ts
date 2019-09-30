81e651183aff93cf7d7d02d49e4ee3e31b05181f
tqma <tqma@ctrip.com>
2019-09-30 13:00:59 +0800

    add strictFunctionTypes

--- options/strictFunctionTypes/false/index.ts
+++ options/strictFunctionTypes/false/index.ts
@@ -16,3 +16,13 @@ declare let f3: (x: Cat) => void;
 f1 = f2;  // Error with --strictFunctionTypes
 f2 = f1;  // Ok
 f2 = f3;  // Error Type '(x: Cat) => void' is not assignable to type '(x: Dog) => void'. Types of parameters 'x' and 'x' are incompatible. Property 'm' is missing in type 'Dog' but required in type 'Cat'.ts(2322)
+
+interface Comparer<T> {
+  compare: (a: T, b: T) => number;
+}
+
+declare let animalComparer: Comparer<Animal>;
+declare let dogComparer: Comparer<Dog>;
+
+animalComparer = dogComparer;
+dogComparer = animalComparer;
