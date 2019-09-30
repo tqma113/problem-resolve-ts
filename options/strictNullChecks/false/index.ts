4260b3ff0d7d6e1335ed4465b97f3f4d9eb9932b
tqma <tqma@ctrip.com>
2019-09-30 12:45:14 +0800

    strictNullChecks

---
+++ options/strictNullChecks/false/index.ts
@@ -0,0 +1,2 @@
+let a1: number = null       // error TS2322: Type 'null' is not assignable to type 'number'.
+let a2: string = undefined  // error TS2322: Type 'undefined' is not assignable to type 'string'
